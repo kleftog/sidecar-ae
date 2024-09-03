a83a388c81440a918094ff939baefc3a9e533405
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-09-03 06:59:21 -0400

    Execute commands fix.

--- tools/run_tab2.sh
+++ tools/run_tab2.sh
@@ -26,10 +26,11 @@ int_benchmarks=("400.perlbench" "401.bzip2" "403.gcc" "429.mcf" "445.gobmk" "456
 
 # Initialize an associative array to hold the benchmark commands
 declare -A benchmark_commands
+declare -A benchmark_binaries
 
 # Create the target directory for copying files
 cpu_usage_dir="$SCRIPT_DIR/../build/cpu-usage"
-rm -r "$cpu_usage_dir"
+rm -rf "$cpu_usage_dir"
 mkdir -p "$cpu_usage_dir"
 
 # Iterate over each integer benchmark in order
@@ -81,6 +82,9 @@ for benchmark in "${int_benchmarks[@]}"; do
                 
                 cp "$build_dir/build_base_$mode".*/"$typemap_basename" "$target_dir/$typemap_target_name"
                 
+                # Store the binary name for later use
+                benchmark_binaries[$benchmark]="$binary_name"
+                
                 # Change directory to the new folder and run gen_tp.sh
                 cd "$target_dir"
                 ../../../sidecar/tools/gen_tp.sh "$binary_name"
@@ -95,6 +99,7 @@ for benchmark in "${int_benchmarks[@]}"; do
     done
 done
 
+# Iterate over benchmarks and run the commands
 for benchmark in "${int_benchmarks[@]}"; do
     if [ -n "${benchmark_commands[$benchmark]}" ]; then
         echo "running $benchmark"
@@ -103,17 +108,19 @@ for benchmark in "${int_benchmarks[@]}"; do
         path=$(echo -e "${benchmark_commands[$benchmark]}" | grep "path:" | awk '{print $2}')
         cmd=$(echo -e "${benchmark_commands[$benchmark]}" | grep "cmd:" | cut -d' ' -f2-)
         
-        # Extract the actual executable name and correct the command
-        executable=$(echo "$cmd" | awk '{print $NF}')
-        cmd=$(echo "$cmd" | sed "s|\.\./run_base_train_sideguard.0000/$executable|./$executable|")
+        # Retrieve the stored binary name
+        binary_name="${benchmark_binaries[$benchmark]}"
+        
+        # Modify the command to start with the binary and remove "../run_base_train_sideguard.0000/"
+        cmd=$(echo "$cmd" | sed "s|\.\./run_base_train_sideguard.0000/$binary_name|./$binary_name|")
 
         # Change to the directory and execute the command
         echo "cd $path"
         cd "$path"
         
         # Run the corrected command
-        echo "./$executable $cmd"
-        ./$executable $cmd
+        echo "./$binary_name $cmd"
+        ./$binary_name $cmd
         
         echo ""
     fi
