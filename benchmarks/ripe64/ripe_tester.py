bc538b92ee8d57b2a1f3f1785d99779a9bcc5974
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-09-06 01:39:02 -0400

    Taskset executable.

--- benchmarks/ripe64/ripe_tester.py
+++ benchmarks/ripe64/ripe_tester.py
@@ -255,7 +255,7 @@ for compiler in compilers:
                                 with subprocess.Popen(
                                     monitorline, shell=True
                                 ) as monitor:
-                                    cmdline = f'(echo "touch /tmp/ripe-eval/f_xxxx" | ./build/{compiler}_attack_gen {parameters_str} >> /tmp/ripe_log 2>&1) 2> /tmp/ripe_log2{i}'
+                                    cmdline = f'taskset -c 0 (echo "touch /tmp/ripe-eval/f_xxxx" | ./build/{compiler}_attack_gen {parameters_str} >> /tmp/ripe_log 2>&1) 2> /tmp/ripe_log2{i}'
                                     os.system(cmdline)
                                     os.wait()
                                     monitor.wait()
@@ -267,7 +267,7 @@ for compiler in compilers:
                                 with subprocess.Popen(
                                     monitorline, shell=True
                                 ) as monitor:
-                                    cmdline = f'(echo "touch /tmp/ripe-eval/f_xxxx" | ./build/{compiler}_attack_gen {parameters_str} >> /tmp/ripe_log 2>&1) 2> /tmp/ripe_log2{i}'
+                                    cmdline = f'taskset -c 0 (echo "touch /tmp/ripe-eval/f_xxxx" | ./build/{compiler}_attack_gen {parameters_str} >> /tmp/ripe_log 2>&1) 2> /tmp/ripe_log2{i}'
                                     os.system(cmdline)
                                     os.wait()
                                     monitor.wait()
