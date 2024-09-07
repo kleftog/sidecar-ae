40e26f7b5ee2413cf09616eba922b0eaa3d5e3d4
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-09-07 04:31:42 -0400

    Slow approach.

--- benchmarks/ripe64/ripe_tester.py
+++ benchmarks/ripe64/ripe_tester.py
@@ -273,7 +273,7 @@ for compiler in compilers:
                                     cmdline = f'echo "touch /tmp/ripe-eval/f_xxxx" | taskset -c 0 ./build/{compiler}_attack_gen {parameters_str} >> /tmp/ripe_log 2>&1 2> /tmp/ripe_log2{i}'
                                     os.system(cmdline)
 
-                                    os.wait()
+                                    time.sleep(1)
 
                                     if check_error(f"/tmp/ripe_log2{i}"):
                                         if psutil.pid_exists(monitor.pid):
