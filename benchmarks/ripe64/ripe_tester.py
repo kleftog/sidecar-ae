4c4eede598746b8f8d6e0ca522f95f12edd3c238
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-09-07 02:51:35 -0400

    Fix.

--- benchmarks/ripe64/ripe_tester.py
+++ benchmarks/ripe64/ripe_tester.py
@@ -268,7 +268,10 @@ for compiler in compilers:
                                 with subprocess.Popen(
                                     monitorline, shell=True
                                 ) as monitor:
-                                    cmdline = f'echo "touch /tmp/ripe-eval/f_xxxx" | taskset -c 0 ./build/{compiler}_attack_gen {parameters_str} >> /tmp/ripe_log 2>&1 2> /tmp/ripe_log2{i}'
+                                    cmdline = (
+                                        f'echo "touch /tmp/ripe-eval/f_xxxx" | taskset -c 0 ./build/{compiler}_attack_gen '
+                                        f"{parameters_str} >> /tmp/ripe_log 2>&1 2> /tmp/ripe_log2{i}"
+                                    )
                                     os.system(cmdline)
 
                                     if check_error(f"/tmp/ripe_log2{i}"):
