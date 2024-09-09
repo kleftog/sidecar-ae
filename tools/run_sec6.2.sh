cfa592ff24ee155d5a407260b3e644ad3b8c954d
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-09-09 12:40:26 -0400

    Increase repeat.

--- tools/run_sec6.2.sh
+++ tools/run_sec6.2.sh
@@ -84,7 +84,7 @@ make clean all
 for mode in "${modes[@]}"; do
     echo "Running ripe_tester for mode: $mode"
     log_file="${CUR_RUN_DIR}/ripe_${mode}.log"
-    if python3 ripe_tester.py both 1 "$mode" > "$log_file" 2>&1; then
+    if python3 ripe_tester.py both 3 "$mode" > "$log_file" 2>&1; then
         echo "Successfully ran ripe_tester for mode: $mode. Log saved to $log_file."
     else
         echo "Error: ripe_tester failed for mode: $mode. Check $log_file for details."
