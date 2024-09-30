390204d7b05b6f9055348d22fc00dd16cacb7f2f
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-09-30 15:15:02 -0400

    Fix paths to monitors.

--- tools/run_sec6.2.sh
+++ tools/run_sec6.2.sh
@@ -67,8 +67,8 @@ function load_ptw_module_int {
 }
 
 # Compile the monitors
-sidecfi_dir="$SCRIPT_DIR/../sidecar/sidecar-monitors/sidecfi"
-sidestack_dir="$SCRIPT_DIR/../sidecar/sidecar-monitors/sidestack"
+sidecfi_dir="$SCRIPT_DIR/../sidecar/sidecar-monitors/sidecfi/x86-64"
+sidestack_dir="$SCRIPT_DIR/../sidecar/sidecar-monitors/sidestack/x86-64"
 cd "$sidecfi_dir" || exit
 make clean all
 cd "$sidestack_dir" || exit
